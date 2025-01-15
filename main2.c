a8e77428c810238478c4c757f2956dfeedd82e23
sodacris <wjxa20152015@gmail.com>
2025-01-15 21:32:46 +0800

    make bar work

--- main2.c
+++ main2.c
@@ -1,44 +1,67 @@
 #define UNICODE
 #define  _UNICODE
 #include <windows.h>
+#include <CommCtrl.h>
+
+static int button_id =1;
+static int p0 = 0;
+HWND g_wndPB;
+static int g_width = 800;
+static int g_height = 600;
+
+static int pbar_width = 400;
+static int pbar_height = 100;
 
 // Window procedure function
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   switch (uMsg) {
-    case WM_CREATE: {
-      // Create a button
-      HWND hwndButton = CreateWindowEx(
+  case WM_CREATE: {
+    RECT rcClient;
+    GetClientRect(hwnd, &rcClient);
+
+    int cyVScroll = GetSystemMetrics(SM_CYVSCROLL);
+
+    HWND hwndPB = CreateWindowEx(
+        0, PROGRESS_CLASS, (LPTSTR)NULL, WS_CHILD | WS_VISIBLE, rcClient.left,
+        rcClient.bottom - cyVScroll, rcClient.right, cyVScroll, hwnd, (HMENU)0,
+        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
+    g_wndPB = hwndPB;
+
+    SendMessage(hwndPB, PBM_SETRANGE, 0, MAKELPARAM(0, 5));
+    SendMessage(hwndPB, PBM_SETSTEP, (WPARAM)1, 0);
+    SendMessage(hwndPB, PBM_SETPOS, 2, 0);
+
+    // Create a button
+    HWND hwndButton = CreateWindowEx(
         0,
-        L"BUTTON",  // Predefined class; Unicode assumed
-        L"OK",      // Button text
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles
-        10,         // x position
-        10,         // y position
-        200,        // Button width
-        40,        // Button height
-        hwnd,     // Parent window
-        (HMENU)1,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-        NULL);      // Pointer not needed.
-      return 0;
-    }
+        L"BUTTON", // Predefined class; Unicode assumed
+        L"OK",     // Button text
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Styles
+        10,                                                    // x position
+        10,                                                    // y position
+        200,                                                   // Button width
+        40,                                                    // Button height
+        hwnd,                                                  // Parent window
+        (HMENU)button_id, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+        NULL); // Pointer not needed.
+    return 0;
+  }
     case WM_COMMAND:
-      if (LOWORD(wParam) == 1) { // Button ID
-        MessageBox(hwnd, L"Button clicked!", L"Information", MB_OK);
+      if (LOWORD(wParam) == button_id) {
+        SendMessage(g_wndPB, PBM_STEPIT, 0, 0);
       }
-      return 0;
-    case WM_USER + 2:
       return 0;
     case WM_DESTROY:
       PostQuitMessage(0);
       return 0;
     default:
       return DefWindowProc(hwnd, uMsg, wParam, lParam);
-  }
+    }
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
                    int nCmdShow) {
+  InitCommonControls();
   // Register the window class
   const wchar_t CLASS_NAME[] = L"MyWindowClass";
 
@@ -52,7 +75,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   RECT r;
   GetClientRect(GetDesktopWindow(), &r);
 
-  RECT clientRect = {0, 0, 800, 600}; // Desired client area size
+  RECT clientRect = {0, 0, g_width, g_height}; // Desired client area size
   DWORD windowStyle = WS_OVERLAPPEDWINDOW;
   DWORD extendedStyle = 0;
 
@@ -68,8 +91,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   HWND hwnd = CreateWindowEx(
     0, // Optional window styles
     CLASS_NAME, // Window class name
-    L"Simple Win32 App", // Window text
-    WS_OVERLAPPEDWINDOW | WS_SYSMENU | WS_CAPTION |WS_VSCROLL |WS_MAXIMIZE, // Window style
+    L"Disk Size Used", // Window text
+    WS_OVERLAPPEDWINDOW, // Window style
 
     // position and size
     pos_x, pos_y, windowWidth, windowHeight,
